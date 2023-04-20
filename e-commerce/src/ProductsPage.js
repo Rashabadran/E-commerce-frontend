82e7a68c98bb1f958892280006d38b3a185c5c42
Rasha <badranrasha685@gmail.com>
2023-04-21 01:37:13 +0300

    Finalizing the Products MainPage

--- e-commerce/src/ProductsPage.js
+++ e-commerce/src/ProductsPage.js
@@ -1,91 +1,50 @@
 import './ProductsPage.css';
 import imagess from './images/jacket.png';
 import imagesHover from './images/Vector.png';
-import React, { useState } from 'react';
-
+import React, { useState ,useEffect} from 'react';
+import axios from 'axios';
 function ProductsPage() {
 
-  const cardData = [
-  {
-    id: 1,
-    title: "Jacket with Hoodie Style",
-    image: imagess,
-    price: 19.99,
-    imageHover:imagesHover,
-  },
-  {
-    id: 2,
-    title: "Jacket with Hoodie Style",
-    image: imagess,
-    price: 29.99,
-    imageHover:imagesHover,
-  },
-  {
-    id: 3,
-    title: "Jacket with Hoodie Style",
-    image: imagess,
-    price: 29.99,
-    imageHover:imagesHover,
-  },
-  {
-    id: 4,
-    title: "Jacket with Hoodie Style",
-    image: imagess,
-    price: 29.99,
-    imageHover:imagesHover,
-  },
-   {
-    id: 5,
-    title: "Jacket with Hoodie Style",
-    image: imagess,
-    price: 29.99,
-    imageHover:imagesHover,
-  },
-   {
-    id: 6,
-    title: "Jacket with Hoodie Style",
-    image: imagess,
-    price: 29.99,
-    imageHover:imagesHover,
-  },
-  // Add more card data objects here as needed
-];
 
 
-const [cardImages, setCardImages] = useState(cardData.map((item) => item.image));
+const [cardDatas, setCardDatas] = useState([]);
+const [cardImages, setCardImages] = useState([]);
 
-  const handleMouseOver = (index) => {
-    const newImages = [...cardImages];
-    newImages[index] = cardData[index].imageHover;
-    setCardImages(newImages);
-  };
 
-  const handleMouseOut = (index) => {
-    const newImages = [...cardImages];
-    newImages[index] = cardData[index].image;
-    setCardImages(newImages);
-  };
 
-  return (
-    <div className='allProducts'>
-      <p className='mainTitleProduct'> Men Jackets</p>
-       <div className="card-list">
-      {cardData.map((item,index)=>{
-        return(
-          <div key={item.id}>
-           <img className="allProductsImage" src={cardImages[index]} alt={item.title} onMouseOver={() => handleMouseOver(index)}
-              onMouseOut={() => handleMouseOut(index)} />
-           <p className='allProductsTitle'>{item.title}</p>
-           <p className='allProductsPrice'>$ {item.price} USD</p>
-           </div>
-           
-        );
-      })}
-    </div>
-    </div>
+ 
+
+  const loadAllProducts = async () => {
+    const res = await axios.get('http://localhost:3030/product/product');
+    setCardDatas(res.data);
+    setCardImages(res.data.map((item,index) => item.image[0]));
    
-     
+  };
+ 
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
   
+  return (
+    <div className="allProducts">
+      <p className="mainTitleProduct">Men Jackets</p>
+      <div className="card-list">
+        {cardDatas.map((item, index) => (
+          <div key={item._id}>
+            <img
+              className="allProductsImage"
+              src={cardImages[index]?.url}
+              alt={item.title}
+             
+            />
+         
+           
+            <p className="allProductsTitle">{item.title}</p>
+            <p className="allProductsPrice">$ {item.price} USD</p>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
